1489f74bb77af9fedbe9f9594e3311513c72bda7
tue <tuevu0171@gmail.com>
2019-11-16 20:52:41 +0700

    feat: theme color palette

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -18,9 +18,48 @@ export enum COLOR {
   'black2' = '#B4B4B4',
 }
 
-export enum PrimaryPalette {
-  'light' = '#fc688e',
-  'main' = '#F62F5E',
-  'dark' = '#cf2557',
-  'contrastText' = '#FFF',
+export const PrimaryPalette = {
+  light: '#fc688e',
+  main: '#F62F5E',
+  dark: '#cf2557',
+  contrastText: '#FFF',
+}
+
+export const BluePalette = {
+  contrastText: '#FFF',
+  dark: '#4592f7',
+  light: '#beddfe',
+  main: '#6EB2FB',
+}
+
+export const BlackPalette = {
+  contrastText: '#FFF',
+  dark: '#2e2e2e',
+  light: '#b0b0b0',
+  main: '#707070',
+}
+
+export const orangePalette = {
+  contrastText: '#FFF',
+  dark: '#ea862e',
+  light: '#f8da4f',
+  main: '#F1AD3D',
+}
+export const grayPalette = {
+  contrastText: '#FFF',
+  dark: '#e5e5e5',
+  light: '#fbfbfb',
+  main: '#F7F7F7',
+}
+export const greenPalette = {
+  contrastText: '#FFF',
+  dark: '#00ad9d',
+  light: '#d4f5f3',
+  main: '#00D3CA',
+}
+export const yellowPalette = {
+  contrastText: '#FFF',
+  dark: '#e4f45e',
+  light: '#f7fccd',
+  main: '#EFFC90',
 }
