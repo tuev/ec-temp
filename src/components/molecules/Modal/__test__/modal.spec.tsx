e440fb0aa2295309a0a64a28fbc3da84a86ac4f6
anhdai <lanhdai1988@gmail.com>
2019-12-02 23:45:48 +0700

    fix(modal): add test for modal

--- src/components/molecules/Modal/__test__/modal.spec.tsx
+++ src/components/molecules/Modal/__test__/modal.spec.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 import { mount } from 'enzyme'
 import Modal from '..'
 
 // refer test modal
 // link: https://medium.com/front-end-weekly/tested-react-build-and-test-modal-using-react-features-and-dom-events-39b7246a3a6f
+
+function WithModal() {
+  const [openModal, setOpenModal] = useState(false)
+  return (
+    <Modal onClose={() => setOpenModal(false)} open={openModal}>
+      <div>
+        Hello World
+        </div>
+    </Modal>
+  )
+}
 
 describe('Modal', () => {
   it('renders modal when open flag is true', () => {
@@ -24,4 +36,11 @@ describe('Modal', () => {
     const title = wrapper.find('.title').text()
     expect(title).toEqual('Modal Title')
   })
+
+  it('closes the Modal when ESC key is pressed', () => {
+    const wrapper = mount(<WithModal />);
+    const evt = new KeyboardEvent('keydown', { keyCode: 27 } as any)
+    document.dispatchEvent(evt)
+    expect(wrapper.find(Modal).props().open).toEqual(false)
+  });
 })
