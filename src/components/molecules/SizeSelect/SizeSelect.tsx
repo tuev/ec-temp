7cbbd8a6014f5e04a99d94886a8e692ca184472b
tue <tuevu0171@gmail.com>
2019-12-14 18:03:01 +0700

    feat: 🎸 add size select molecule

---
+++ src/components/molecules/SizeSelect/SizeSelect.tsx
@@ -0,0 +1,34 @@
+import React, { FC, useCallback } from 'react'
+import { BasicSizeSelectProps } from './Size.type'
+import Button from 'components/atoms/Button'
+import { SizeSelectWrapper, SizeItemWrapper } from './Size.styled'
+
+export const defaultMethod = () => true
+
+const SizeSelect: FC<BasicSizeSelectProps> = props => {
+  const { sizes = [], value, onChange = defaultMethod } = props
+  const handleSizeOnChange = useCallback(
+    (size: string) => () => onChange(size),
+    [onChange]
+  )
+
+  return (
+    <SizeSelectWrapper>
+      {sizes.map(size => (
+        <SizeItemWrapper key={size}>
+          <Button
+            mx="4px"
+            color={value === size ? 'secondary' : 'default'}
+            onClick={handleSizeOnChange(size)}
+            size="small"
+            data-testid={`size-filter-${size}`}
+          >
+            {size}
+          </Button>
+        </SizeItemWrapper>
+      ))}
+    </SizeSelectWrapper>
+  )
+}
+
+export default SizeSelect
