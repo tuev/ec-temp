5de88839e5b2148737f6de18fea2e7be7bfc64c4
anhdai <lanhdai1988@gmail.com>
2020-02-03 23:23:30 +0700

    feat(org/add-review): review form

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -2,5 +2,7 @@ import BlogIcon from './BlogIcon'
 import Input from './Input'
 import TextArea from './TextArea'
 import Wrapper from './Wrapper'
+import Rating from './Rating'
+import Button from './Button'
 
-export { BlogIcon, Input, TextArea, Wrapper }
+export { BlogIcon, Input, TextArea, Wrapper, Rating, Button }
